d540bbf3b1c770b24df8dfa004801060e40c8012
Kachi Alex <opd.livmind@gmail.com>
2025-10-16 07:36:42 +0100

    Fixed z-index for nurse report modal to appear on top

--- src/components/NurseReportGenerator.js
+++ src/components/NurseReportGenerator.js
@@ -337,7 +337,7 @@ ${formData.additionalNotes}
   };
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-[60] p-4">
       <div className="bg-white rounded-xl shadow-2xl max-w-6xl w-full max-h-[90vh] overflow-y-auto">
         <div className="p-6 border-b border-gray-200">
           <div className="flex items-center justify-between">
